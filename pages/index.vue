b0efe0c3d89a0c6447b5041bbc33a8ca01631e66
Leo Crapart <leocrapart@yahoo.fr>
2022-06-04 22:14:45 +0200

    nuxt + cv

---
+++ pages/index.vue
@@ -0,0 +1,115 @@
+<template>
+  <div class="bg-gray-100">
+    <div class="flex flex-wrap">
+      <div class="relative w-full lg:w-1/2 xl:w-1/3">
+        <div class="relative" style="padding-bottom: 60%">
+          <img
+            class="absolute h-full w-full object-cover"
+            src="assets/preview1.jpg"
+            alt=""
+          />
+        </div>
+        <a href="https://mixd.co.uk" class="flex">
+          <div
+            class="
+              absolute
+              bottom-0
+              py-1
+              hover:bg-gray-500
+              w-full
+              text-center
+              duration-500
+              ease-in-out
+            "
+          >
+            mixd.co.uk
+          </div>
+        </a>
+      </div>
+
+      <div class="relative w-full lg:w-1/2 xl:w-1/3">
+        <div class="relative" style="padding-bottom: 60%">
+          <img
+            class="absolute h-full w-full object-cover"
+            src="assets/preview2.jpg"
+            alt=""
+          />
+        </div>
+        <a href="https://clay.global" class="flex">
+          <div
+            class="
+              absolute
+              bottom-0
+              py-1
+              hover:bg-gray-500
+              w-full
+              text-center
+              duration-500
+              ease-in-out
+            "
+          >
+            clay.global
+          </div>
+        </a>
+      </div>
+
+      <div class="relative w-full lg:w-1/2 xl:w-1/3">
+        <div class="relative" style="padding-bottom: 60%">
+          <img
+            class="absolute h-full w-full object-cover"
+            src="assets/preview3.jpg"
+            alt=""
+          />
+        </div>
+        <a href="https://www.designory.com/" class="flex">
+          <div
+            class="
+              absolute
+              bottom-0
+              py-1
+              hover:bg-gray-500
+              w-full
+              text-center
+              duration-500
+              ease-in-out
+            "
+          >
+            designory.com
+          </div>
+        </a>
+      </div>
+
+      <div class="relative w-full lg:w-1/2 xl:w-1/3">
+        <div class="relative" style="padding-bottom: 60%">
+          <img
+            class="absolute h-full w-full object-cover"
+            src="assets/preview4.jpg"
+            alt=""
+          />
+        </div>
+        <a href="https://cliquestudios.com/" class="flex">
+          <div
+            class="
+              absolute
+              bottom-0
+              py-1
+              hover:bg-gray-500
+              w-full
+              text-center
+              duration-500
+              ease-in-out
+            "
+          >
+            cliquestudios.com
+          </div>
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "IndexPage",
+};
+</script>
