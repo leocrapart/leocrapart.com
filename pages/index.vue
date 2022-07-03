1885b3a83d9dba5b6ec5151500f252e6d10636ad
Leo Crapart <leocrapart@yahoo.fr>
2022-07-04 01:28:09 +0200

    treecoin

--- pages/index.vue
+++ pages/index.vue
@@ -104,6 +104,33 @@
           </div>
         </a>
       </div>
+
+
+      <div>Fast editor : don't wait to code</div>
+      <div>Simple tools : no more learn all of this to code</div>
+      <div>Code elegance : no more complicated ununderstandable things</div>
+      <div>Code speed : program runs fast</div>
+
+      <div>Treecoin : crypto x ecology</div>
+      <div>Trees : the more the crypto is used, the more trees are planted (ecosia)</div>
+      <div>Ecologic : gaz impact is low, don't need to run a bunch of calculus to make the transaction, but still is a cryptocurrency (performance)</div>
+      <div>Scalable : should be capable to handle a lot of transaction (scalability)</div>
+      <div>Public friendly : anybody wanting to use treecoin should be able to do so easily, no headache, not for crypto traders only (easy ui, buy from fiat)</div>
+      <div>Meaningful : treecoin value should have some meaning, not 0.0001$, nether 50k$, the value reflects something real so that having one treecoin means something real, a forest, a tree, a seed, user should see the concrete effects of his usage of the treecoin, like brave and ecosia (tangibility)</div>
+      <div>Clojure/script + 1 for very hot spots + 1 for frontend (multipage/mobile-app), but stay in clojure as far as possible (re-frame for web, react-native for mobile)</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
     </div>
   </div>
 </template>
